462f6fbc28f4fc695df0e5c6239f8afe10932bf1
Naghmanaimran <imran2244ansari@gmail.com>
2024-03-31 22:57:09 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todos:string[]= [];
+let condition = true;
+while(condition){
+
+let addtask = await inquirer.prompt(
+    [
+        {
+            name : "todo",
+            type : "input",
+            message :chalk.bgRedBright("what do you want in  add your todo? ")
+        },
+        {
+            name : "addmore",
+            type: "confirm",
+            message :chalk.greenBright("do want to add more Todo?"),
+            default: "false"
+        }
+
+
+    ]
+);
+todos.push(addtask.todo);
+condition = addtask.addmore;
+console.log(todos);
+}
